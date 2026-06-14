e1c9e55419dc27a8f2e8b74718cb43545a1fb1df
Giorgi Meshveliani <giorgimeshveliani03@gmail.com>
2026-06-14 23:37:01 +0400

    fix og-image png

---
+++ scripts/svg-to-png.mjs
@@ -0,0 +1,6 @@
+﻿import sharp from 'sharp';
+import { readFileSync } from 'fs';
+
+const svg = readFileSync('./src/assets/og-image.svg');
+await sharp(svg).resize(1200, 630).png().toFile('./src/assets/og-image.png');
+console.log('og-image.png შექმნილია!');
